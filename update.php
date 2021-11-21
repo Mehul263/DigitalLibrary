4b683c5f042c41895d9e77c7b59ce5e2e413845a
Mehul263 <75162693+Mehul263@users.noreply.github.com>
2021-11-21 16:57:41 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,16 @@
+<?php
+
+$con = mysqli_connect('localhost','root','','bookdb');
+$email = $_POST['email_id'];
+$password = $_POST['password'];
+
+$check = mysqli_query($con,"UPDATE userdetails SET password = '$password' WHERE email = '$email_id'")
+
+if($check>0){
+    echo "Data Added";
+} else{
+    echo "Error";
+    }
+
+    
+?>
